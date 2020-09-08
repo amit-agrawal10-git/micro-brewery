a48755b652a16988e99f5b83f55d5db567a93da9
amit-agrawal10 <amit.agrawal10@gmail.com>
2020-09-08 20:23:23 +0530

    .

---
+++ src/test/java/spring/micro/brewery/BreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.micro.brewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
